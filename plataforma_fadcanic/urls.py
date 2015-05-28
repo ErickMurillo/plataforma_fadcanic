fae25e410aa2c8d896e855ab364f92f5db31daa4
Carlos Rocha <crocha09.09@gmail.com>
2015-05-27 22:22:58 -0600

    fix urls

--- plataforma_fadcanic/urls.py
+++ plataforma_fadcanic/urls.py
@@ -17,9 +17,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from cambiaahora.noticias import urls as noticias_urls
 
 urlpatterns = [
-	url(r'^noticias/', include('noticias.urls')),
+	url(r'', include(noticias_urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^ckeditor/', include('ckeditor.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
