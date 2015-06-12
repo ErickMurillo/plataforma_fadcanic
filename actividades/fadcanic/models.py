c9a105dae13e7e4454db14e9894afe82139c66ff
Erick Murillo <erickmurillo22@gmail.com>
2015-06-12 12:56:49 -0600

    --

--- actividades/fadcanic/models.py
+++ actividades/fadcanic/models.py
@@ -3,7 +3,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from actividades.thumbs import ImageWithThumbsField
 from actividades.utils import get_file_path
-import datetime 
+import datetime
+from sorl.thumbnail import ImageField 
 
 class ResultadoPrograma(models.Model):    
     nombre_corto = models.CharField(max_length=50)
@@ -24,11 +25,11 @@ class Organizacion(models.Model):
     telefono = models.CharField(max_length=12, blank=True, default='')
     direccion = models.CharField(max_length=300, blank=True, default='')
     web = models.URLField(verbose_name=u'Sitio web', blank=True, default='www.example.com')
-    historia = models.TextField(blank=True, default='')        
-    logo = ImageWithThumbsField(upload_to=get_file_path, sizes=((50, 50),), blank=True, null=True) 
+    historia = models.TextField(blank=True, default='')      
+    logo = ImageField(upload_to=get_file_path,blank=True, null=True)
     last_register = models.DateTimeField(editable=False, default=datetime.datetime.now())
     
-    fileDir = 'logos'
+    fileDir = 'logos/'
     
     def __unicode__(self):
         return u'%s' % self.nombre_corto
