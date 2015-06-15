22a3a8cbbf21609ae1b71302342146a03b3b909b
Carlos Rocha <crocha09.09@gmail.com>
2015-06-15 11:19:21 -0600

    agregando fix alos urls

--- cambiaahora/noticias/urls.py
+++ cambiaahora/noticias/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	#url(r'^$', views.IndexView.as_view(), name='index'),
+	url(r'^$', views.IndexView.as_view(), name='index'),
 	url(r'^noticias/$', views.ListNewsView.as_view(), name='noticias-list'),
 	url(r'^(?P<slug>[-\w]+)/$', views.DetailNewsView.as_view(), name='noticia-detail'),
     
--- plataforma_fadcanic/urls.py
+++ plataforma_fadcanic/urls.py
@@ -22,8 +22,7 @@ from django.conf.urls.i18n import i18n_patterns
 from cambiaahora.noticias import views
 
 urlpatterns = [
-	#url(r'', include(noticias_urls)),
-    url(r'^$', views.IndexView.as_view(), name='index-carajo'),
+	url(r'', include(noticias_urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^ckeditor/', include('ckeditor.urls')),
     url(r'^actividades/', include('actividades.urls')),
