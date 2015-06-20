698715606e668b4db1d7d5caf5cbae3be88794ae
Carlos Rocha <crocha09.09@gmail.com>
2015-06-20 00:58:33 -0600

    agregando el ckeditor al flatpages

--- cambiaahora/historias/admin.py
+++ cambiaahora/historias/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
+from django import forms
 from .models import Historias
+from django.contrib.flatpages.models import FlatPage
+# Note: we are renaming the original Admin and Form as we import them!
+from django.contrib.flatpages.admin import FlatPageAdmin as FlatPageAdminOld
+from django.contrib.flatpages.forms import FlatpageForm as FlatpageFormOld
 
+from ckeditor.widgets import CKEditorWidget
+ 
+class FlatpageForm(FlatpageFormOld):
+    content = forms.CharField(widget=CKEditorWidget())
+    class Meta:
+        model = FlatPage # this is not automatically inherited from FlatpageFormOld
+        fields = '__all__'
+ 
+ 
+class FlatPageAdmin(FlatPageAdminOld):
+    form = FlatpageForm
+ 
 class HistoriasAdmin(admin.ModelAdmin):
 	def queryset(self, request):
 		if request.user.is_superuser:
@@ -18,3 +35,6 @@ class HistoriasAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Historias, HistoriasAdmin)
+# We have to unregister the normal admin, and then reregister ours
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdmin)
