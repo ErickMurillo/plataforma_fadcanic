97dd60965e36b48a9a515a393427b658d7af1bd2
Erick Murillo <erickmurillo22@gmail.com>
2015-06-30 16:32:38 -0600

    salidas monitoreo actividades

--- monitoreo/comunidad/urls.py
+++ monitoreo/comunidad/urls.py
@@ -1,11 +1,6 @@
 from django.conf.urls import url
-from . import views
+from .views import *
 
 urlpatterns = [
-	url(r'^$', views.MonitoreoView.as_view(), name='monitoreo'),
-	# url(r'^lang/(?P<lang_code>\w+)/$', views.set_lang, name='set_lang'),
-	# url(r'^noticias/$', views.ListNewsView.as_view(), name='noticias-list'),
-	# url(r'^noticias/(?P<slug>[-\w]+)/$', views.DetailNewsView.as_view(), name='noticia-detail'),
-	#url(r'^contactenos/$', views.ContactView.as_view(), name='contactenos'),
-    
+	url(r'^$', MonitoreoView.as_view(), name='monitoreo'),
 ]
