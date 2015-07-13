67e9482a16c127c75cb24cb3290070e1785dc122
Carlos Rocha <crocha09.09@gmail.com>
2015-07-13 13:30:42 -0600

    agregando un documental mas en el index

--- cambiaahora/noticias/views.py
+++ cambiaahora/noticias/views.py
@@ -40,7 +40,7 @@ class IndexView(TemplateView):
             context['albunes'] = Fotos.objects.filter(aprobacion=2,idioma=2).order_by('id')[:1]
             context['audios'] = Audios.objects.filter(aprobacion=2,idioma=2).order_by('-id')[:3]
             context['videos'] = Videos.objects.filter(aprobacion=2,idioma=2).order_by('id')[:3]
-            context['documentales'] = Documentales.objects.filter(aprobacion=2,idioma=2).order_by('fecha')[:3]
+            context['documentales'] = Documentales.objects.filter(aprobacion=2,idioma=2).order_by('fecha')[:4]
             context['informacion'] = Informacion.objects.filter(idioma=2).order_by('id')[:1]
         else:
             context['ultimas_noticias'] = Noticias.objects.filter(aprobacion=2,idioma=1).order_by('-fecha')[:6]
@@ -49,7 +49,7 @@ class IndexView(TemplateView):
             context['albunes'] = Fotos.objects.filter(aprobacion=2,idioma=1).order_by('id')[:1]
             context['audios'] = Audios.objects.filter(aprobacion=2,idioma=1).order_by('-id')[:3]
             context['videos'] = Videos.objects.filter(aprobacion=2,idioma=1).order_by('id')[:3]
-            context['documentales'] = Documentales.objects.filter(aprobacion=2,idioma=1).order_by('fecha')[:3]
+            context['documentales'] = Documentales.objects.filter(aprobacion=2,idioma=1).order_by('fecha')[:4]
             context['informacion'] = Informacion.objects.filter(id=1, idioma=1)
         
         context['config'] = Configuracion.objects.all()[:3]
