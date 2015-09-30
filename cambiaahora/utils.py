9bf735bbb3b17fe92f4608335bdae786499063b2
Carlos Rocha <crocha09.09@gmail.com>
2015-09-30 15:28:11 -0600

    fix mail arreglo

--- cambiaahora/utils.py
+++ cambiaahora/utils.py
@@ -1,7 +1,8 @@
 import os
 
-arreglo_mail = ['crocha09.09@gmail.com','pablo.ismael90@gmail.com',
-				'erickmurillo22@gmail.com','mcu.caps@gmail.com']
+arreglo_mail = ['crocha09.09@gmail.com','keniaregina@gmail.com',
+				'delrysimons@yahoo.com','changenowlestsdoit@gmail.com',
+				'vgo1993@yahoo.es','vgonzalez@fadcanic.org.ni']
 
 def get_file_path(intance,filename):
     return os.path.join(intance.fileDir, filename)
