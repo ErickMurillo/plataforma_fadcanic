f7a9feb02f2a3aa57a126976799ec7b094bafe9e
Carlos Rocha <crocha09.09@gmail.com>
2015-10-07 17:54:24 -0600

    fix pdf template

--- actividades/views.py
+++ actividades/views.py
@@ -94,44 +94,44 @@ def obtener_lista(request):
         serializado = simplejson.dumps(lista)
         return HttpResponse(serializado, content_type='application/json')
 
-# def datos(request,template='monitoreo_actividades/datos.html'):
-# 	municipios = {}
-# 	for y in Municipio.objects.all():
-# 		sum_hombres = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('hombres'))
-# 		sum_mujeres = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('mujeres'))
-# 		#edad
-# 		sum_menor_12 = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('menor_12'))
-# 		sum_mayor_12 = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('mayor_12'))
-# 		sum_mayor_18 = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('mayor_18'))
-# 		sum_mayor_30 = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('mayor_30'))
-# 		#identidad etnica
-# 		sum_creole = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('creole'))
-# 		sum_miskito = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('miskito'))
-# 		sum_ulwa = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('ulwa'))
-# 		sum_rama = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('rama'))
-# 		sum_mestizo = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('mestizo'))
-# 		sum_mayagna = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('mayagna'))
-# 		sum_garifuna = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('garifuna'))
-# 		sum_extranjero = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('extranjero'))
-# 		#tipos de actores
-# 		sum_estudiante = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('estudiante'))
-# 		sum_docente = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('docente'))
-# 		sum_periodista = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('periodista'))
-# 		sum_lideres = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('lideres'))
-# 		sum_representantes = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('representantes'))
-# 		sum_autoridades = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('autoridades'))
-# 		sum_comunitarios = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('comunitarios'))
+def datos(request,template='monitoreo_actividades/datos.html'):
+	municipios = {}
+	for y in Municipio.objects.all():
+		sum_hombres = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('hombres'))
+		sum_mujeres = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('mujeres'))
+		#edad
+		sum_menor_12 = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('menor_12'))
+		sum_mayor_12 = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('mayor_12'))
+		sum_mayor_18 = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('mayor_18'))
+		sum_mayor_30 = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('mayor_30'))
+		#identidad etnica
+		sum_creole = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('creole'))
+		sum_miskito = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('miskito'))
+		sum_ulwa = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('ulwa'))
+		sum_rama = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('rama'))
+		sum_mestizo = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('mestizo'))
+		sum_mayagna = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('mayagna'))
+		sum_garifuna = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('garifuna'))
+		sum_extranjero = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('extranjero'))
+		#tipos de actores
+		sum_estudiante = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('estudiante'))
+		sum_docente = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('docente'))
+		sum_periodista = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('periodista'))
+		sum_lideres = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('lideres'))
+		sum_representantes = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('representantes'))
+		sum_autoridades = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('autoridades'))
+		sum_comunitarios = Actividad.objects.filter(municipio__nombre=y.nombre).aggregate(Sum('comunitarios'))
 
-# 		actividades = Actividad.objects.filter(municipio__nombre=y.nombre).count()
+		actividades = Actividad.objects.filter(municipio__nombre=y.nombre).count()
 
-# 		if actividades != 0:
-# 			municipios[y.nombre] = (actividades,sum_hombres,sum_mujeres,sum_menor_12,sum_mayor_12,
-# 										sum_mayor_18,sum_mayor_30,sum_creole,sum_miskito,sum_ulwa,
-# 										sum_rama,sum_mestizo,sum_mayagna,sum_garifuna,sum_extranjero,
-# 										sum_estudiante,sum_docente,sum_periodista,
-# 										sum_representantes,sum_autoridades,sum_comunitarios)
+		if actividades != 0:
+			municipios[y.nombre] = (actividades,sum_hombres,sum_mujeres,sum_menor_12,sum_mayor_12,
+										sum_mayor_18,sum_mayor_30,sum_creole,sum_miskito,sum_ulwa,
+										sum_rama,sum_mestizo,sum_mayagna,sum_garifuna,sum_extranjero,
+										sum_estudiante,sum_docente,sum_periodista,
+										sum_representantes,sum_autoridades,sum_comunitarios)
 
-# 	return render(request, template, locals())
+	return render(request, template, locals())
 
 def render_to_pdf(template_src, context_dict):
     template = get_template(template_src)
