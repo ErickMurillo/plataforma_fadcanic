e673459d205f750dca770196e7f951259da1fca6
Erick Murillo <erickmurillo22@gmail.com>
2015-11-30 08:41:09 -0600

    ---

--- static_media/mapeo/js/admin.js
+++ static_media/mapeo/js/admin.js
@@ -1,5 +1,53 @@
 (function($) {	
 	$(document).ready(function(){
+		var valor_tipo = $('#id_tipo').val();
+			if (valor_tipo == '1' ) {
+				$('.field-nombre').show();
+				$('.field-departamento').show();
+				$('.field-municipio').show();
+				$('.field-persona_contacto').show();
+				$('.field-direccion').show();
+				$('.field-celular').show();
+				$('.field-convencional').show();
+				$('.field-correo').show();
+				$('.field-web').show();
+				$('.field-facebook').show();
+				$('.field-twitter').show();
+				$('.field-youtube').show();
+				$('.field-otro').show();
+				$('.field-cobertura').show();
+				$('.field-acciones_violencia').show();
+				$('.field-acciones_apoyo').hide();
+				$('.field-comunidad').hide();
+				$('.field-masculino').hide();
+				$('.field-femenino').hide();
+				$('.field-integrantes').hide();
+				$('.field-mayor_13').hide();
+				$('.field-mayor_19').hide();
+				$('.field-mayor_30').hide();
+			
+			}else if (valor_tipo != '1'){
+				$('.field-persona_contacto').hide();
+				$('.field-direccion').hide();
+				$('.field-celular').hide();
+				$('.field-convencional').hide();
+				$('.field-correo').hide();
+				$('.field-web').hide();
+				$('.field-facebook').hide();
+				$('.field-twitter').hide();
+				$('.field-youtube').hide();
+				$('.field-otro').hide();
+				$('.field-cobertura').hide();
+				$('.field-acciones_apoyo').show();
+				$('.field-comunidad').show();
+				$('.field-masculino').show();
+				$('.field-femenino').show();
+				$('.field-integrantes').show();
+				$('.field-mayor_13').show();
+				$('.field-mayor_19').show();
+				$('.field-mayor_30').show();
+			};
+			
 		$('#id_tipo').change(function(){
 			var valor_tipo = $('#id_tipo').val();
 			if (valor_tipo == '1' ) {
