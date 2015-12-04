ae027cda36852da86e16796c675a2894c7c32760
Erick Murillo <erickmurillo22@gmail.com>
2015-12-04 14:59:52 -0600

    salidas de conocimiento

--- violencia_juvenil/views.py
+++ violencia_juvenil/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from .forms import *
+import collections
 
 # Create your views here.
 def _queryset_filtrado(request):
@@ -25,6 +26,7 @@ def index(request,template="violencia_juvenil/index.html"):
 		if form.is_valid():
 			request.session['year'] = form.cleaned_data['year']
 			year = request.session['year']
+			encuestas = Encuesta.objects.filter(year=year).count()
 			try:
 				#sexo----------------
 				sexo = {}
@@ -40,13 +42,45 @@ def index(request,template="violencia_juvenil/index.html"):
 				etnia = {}
 				for x in ETNIAS_CHOICES:
 					count_etnia = InformacionEntrevistado.objects.filter(encuesta__year=year,etnia=x[0]).count()
-					etnia[x[1]] = count_etnia
-				#fin salidas---------
+					etnia[x[1]] = saca_porcentajes(count_etnia,encuestas,False)
+
+				#tab conocimiento--------------------------------------------------
+				#preg 3
+				pregunta3 = collections.OrderedDict()
+				for obj in CHOICE_TIPO_VIOLENCIA_3:
+					conteo = Conocimiento.objects.filter(pregunta3__contains=obj[0],encuesta__year=year).count()
+					pregunta3[obj[1]] = saca_porcentajes(conteo,encuestas,False)
+
+				#preg 4
+				pregunta4 = collections.OrderedDict()
+				for obj in CHOICE_LUGARES_4:
+					conteo = Conocimiento.objects.filter(pregunta4__contains=obj[0],encuesta__year=year).count()
+					pregunta4[obj[1]] = saca_porcentajes(conteo,encuestas,False)
+
+				#preg 7
+				pregunta7 = collections.OrderedDict()
+				for obj in CHOICE_SOLUCIONES_7:
+					conteo = Conocimiento.objects.filter(pregunta7__contains=obj[0],encuesta__year=year).count()
+					pregunta7[obj[1]] = saca_porcentajes(conteo,encuestas,False)
+
+				#preg 9
+				pregunta9 = collections.OrderedDict()
+				for obj in CHOICE_FEMINICIDIO_9:
+					conteo = Conocimiento.objects.filter(pregunta9__contains=obj[0],encuesta__year=year).count()
+					pregunta9[obj[1]] = saca_porcentajes(conteo,encuestas,False)
+
+				#preg 13
+				pregunta13 = collections.OrderedDict()
+				for obj in CHOICE_SEGURIDAD_CIUDADA_13:
+					conteo = Conocimiento.objects.filter(pregunta13__contains=obj[0],encuesta__year=year).count()
+					pregunta13[obj[1]] = saca_porcentajes(conteo,encuestas,False)
+				#fin salidas-------------------------------------------------------
 
 			except:
 				pass
 	else:
 		form = ViolenciaConsulta()
+		encuestas = Encuesta.objects.count()
 		try:
 			#sexo----------------
 			sexo = {}
@@ -59,14 +93,58 @@ def index(request,template="violencia_juvenil/index.html"):
 				count_edad = InformacionEntrevistado.objects.filter(edad=x[0]).count()
 				edad[x[1]] = count_edad
 			#etnia---------------
-				etnia = {}
-				for x in ETNIAS_CHOICES:
-					count_etnia = InformacionEntrevistado.objects.filter(encuesta__year=year,etnia=x[0]).count()
-					etnia[x[1]] = count_etnia
-			#fin salidas---------
+			etnia = {}
+			for x in ETNIAS_CHOICES:
+				count_etnia = InformacionEntrevistado.objects.filter(etnia=x[0]).count()
+				etnia[x[1]] = saca_porcentajes(count_etnia,encuestas,False)
+
+			#tab conocimiento--------------------------------------------------
+			#preg 3
+			pregunta3 = collections.OrderedDict()
+			for obj in CHOICE_TIPO_VIOLENCIA_3:
+				conteo = Conocimiento.objects.filter(pregunta3__contains=obj[0]).count()
+				pregunta3[obj[1]] = saca_porcentajes(conteo,encuestas,False)
+
+			#preg 4
+			pregunta4 = collections.OrderedDict()
+			for obj in CHOICE_LUGARES_4:
+				conteo = Conocimiento.objects.filter(pregunta4__contains=obj[0]).count()
+				pregunta4[obj[1]] = saca_porcentajes(conteo,encuestas,False)
+
+			#preg 7	
+			pregunta7 = collections.OrderedDict()
+			for obj in CHOICE_SOLUCIONES_7:
+				conteo = Conocimiento.objects.filter(pregunta7__contains=obj[0]).count()
+				pregunta7[obj[1]] = saca_porcentajes(conteo,encuestas,False)
+
+			#preg 9
+			pregunta9 = collections.OrderedDict()
+			for obj in CHOICE_FEMINICIDIO_9:
+				conteo = Conocimiento.objects.filter(pregunta9__contains=obj[0]).count()
+				pregunta9[obj[1]] = saca_porcentajes(conteo,encuestas,False)
+
+			#preg 13
+				pregunta13 = collections.OrderedDict()
+				for obj in CHOICE_SEGURIDAD_CIUDADA_13:
+					conteo = Conocimiento.objects.filter(pregunta13__contains=obj[0]).count()
+					pregunta13[obj[1]] = saca_porcentajes(conteo,encuestas,False)
+			#fin salidas-------------------------------------------------------
 
 			del request.session['tipo']
 		except:
 			pass
 
 	return render(request, template, locals())
+
+def saca_porcentajes(dato, total, formato=True):
+    if dato != None:
+        try:
+            porcentaje = (dato/float(total)) * 100 if total != None or total != 0 else 0
+        except:
+            return 0
+        if formato:
+            return porcentaje
+        else:
+            return '%.2f' % porcentaje
+    else:
+        return 0
