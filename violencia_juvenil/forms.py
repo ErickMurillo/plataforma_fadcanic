47c0d76aea4b160998f01b4dfc239d2c29894c70
Erick Murillo <erickmurillo22@gmail.com>
2016-01-19 12:59:37 -0600

    filtros violencia

--- violencia_juvenil/forms.py
+++ violencia_juvenil/forms.py
@@ -9,7 +9,16 @@ def fecha_choice():
         years.append((en,en))
     return list(sorted(set(years)))
 
+def departamentos():   
+    foo = Encuesta.objects.all().order_by('informacionentrevistado__municipio1__departamento__nombre').distinct().values_list('informacionentrevistado__municipio1__departamento__id', flat=True)
+    return Departamento.objects.filter(id__in=foo)
+
 class ViolenciaConsulta(forms.Form):
     def __init__(self, *args, **kwargs):
         super(ViolenciaConsulta, self).__init__(*args, **kwargs)
         self.fields['year'] = forms.ChoiceField(label=u'Año',choices=fecha_choice(),required=False)
+        self.fields['grupos'] = forms.ChoiceField(label=u'Grupos',choices=GRUPOS_CHOICES,required=False)
+        self.fields['departamento'] = forms.ModelMultipleChoiceField(queryset=departamentos(), required=False,
+        															 label=u'Departamentos')
+        self.fields['municipio'] = forms.ModelMultipleChoiceField(queryset=Municipio.objects.all().order_by('nombre'), 
+        															required=False, label='Municipios')
--- violencia_juvenil/views.py
+++ violencia_juvenil/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import *
 from .forms import *
 import collections
+import json as simplejson
+from django.http import HttpResponse
 
 # Create your views here.
 def _queryset_filtrado(request):
@@ -9,6 +11,16 @@ def _queryset_filtrado(request):
 
 	if request.session['year']:
 		params['year'] = request.session['year']
+
+	if request.session['grupos']:
+		params['grupos'] = request.session['grupos']
+
+	if request.session['departamento']:
+		if not request.session['municipio']:
+			municipios = Municipio.objects.filter(departamento__in=request.session['departamento'])
+			params['informacionentrevistado__municipio1__in'] = municipios
+		else:
+			params['informacionentrevistado__municipio1__in'] = request.session['municipio']
 
 	unvalid_keys = []
 	for key in params:
@@ -25,106 +37,111 @@ def index(request,template="violencia_juvenil/index.html"):
 		form = ViolenciaConsulta(request.POST)
 		if form.is_valid():
 			request.session['year'] = form.cleaned_data['year']
-			year = request.session['year']
-			encuestas = Encuesta.objects.filter(year=year).count()
+			request.session['grupos'] = form.cleaned_data['grupos']
+			request.session['departamento'] = form.cleaned_data['departamento']
+			request.session['municipio'] = form.cleaned_data['municipio']
+			
+			filtro = _queryset_filtrado(request)
+			encuestas = filtro.count()
+
 			try:
 				#sexo----------------
 				sexo = {}
 				for x in CHOICE_SEXO:
-					count_sexo = InformacionEntrevistado.objects.filter(encuesta__year=year,sexo=x[0]).count()
+					count_sexo = filtro.filter(informacionentrevistado__sexo=x[0]).count()
 					sexo[x[1]] = count_sexo
 				#edad----------------
 				edad = {}
 				for x in EDAD_CHOICES:
-					count_edad = InformacionEntrevistado.objects.filter(encuesta__year=year,edad=x[0]).count()
+					count_edad = filtro.filter(informacionentrevistado__edad=x[0]).count()
 					edad[x[1]] = count_edad
 				#etnia---------------
 				etnia = {}
 				for x in ETNIAS_CHOICES:
-					count_etnia = InformacionEntrevistado.objects.filter(encuesta__year=year,etnia=x[0]).count()
+					count_etnia = filtro.filter(informacionentrevistado__etnia=x[0]).count()
 					etnia[x[1]] = saca_porcentajes(count_etnia,encuestas,False)
 
 				#tab conocimiento--------------------------------------------------
 				#preg 3
 				pregunta3 = collections.OrderedDict()
 				for obj in CHOICE_TIPO_VIOLENCIA_3:
-					conteo = Conocimiento.objects.filter(pregunta3__contains=obj[0],encuesta__year=year).count()
+					conteo = filtro.filter(conocimiento__pregunta3__contains=obj[0]).count()
 					pregunta3[obj[1]] = saca_porcentajes(conteo,encuestas,False)
 
 				#preg 4
 				pregunta4 = collections.OrderedDict()
 				for obj in CHOICE_LUGARES_4:
-					conteo = Conocimiento.objects.filter(pregunta4__contains=obj[0],encuesta__year=year).count()
+					conteo = filtro.filter(conocimiento__pregunta4__contains=obj[0]).count()
 					pregunta4[obj[1]] = saca_porcentajes(conteo,encuestas,False)
 
 				#preg 7
 				pregunta7 = collections.OrderedDict()
 				for obj in CHOICE_SOLUCIONES_7:
-					conteo = Conocimiento.objects.filter(pregunta7__contains=obj[0],encuesta__year=year).count()
+					conteo = filtro.filter(conocimiento__pregunta7__contains=obj[0]).count()
 					pregunta7[obj[1]] = saca_porcentajes(conteo,encuestas,False)
 
 				#preg 9
 				pregunta9 = collections.OrderedDict()
 				for obj in CHOICE_FEMINICIDIO_9:
-					conteo = Conocimiento.objects.filter(pregunta9__contains=obj[0],encuesta__year=year).count()
+					conteo = filtro.filter(conocimiento__pregunta9__contains=obj[0]).count()
 					pregunta9[obj[1]] = saca_porcentajes(conteo,encuestas,False)
 
 				#preg 13
 				pregunta13 = collections.OrderedDict()
 				for obj in CHOICE_SEGURIDAD_CIUDADA_13:
-					conteo = Conocimiento.objects.filter(pregunta13__contains=obj[0],encuesta__year=year).count()
+					conteo = filtro.filter(conocimiento__pregunta13__contains=obj[0]).count()
 					pregunta13[obj[1]] = saca_porcentajes(conteo,encuestas,False)
 
 				#tab actitud-------------------------------------------------------
 				#preg 15
 				pregunta15 = collections.OrderedDict()
 				for obj in CHOICE_15:
-					conteo = Actitud.objects.filter(pregunta15__contains=obj[0],encuesta__year=year).count()
+					conteo = filtro.filter(actitud__pregunta15__contains=obj[0]).count()
 					pregunta15[obj[1]] = saca_porcentajes(conteo,encuestas,False)
 
 				#preg 19
 				pregunta19 = collections.OrderedDict()
 				for obj in CHOICE_19:
-					conteo = Actitud.objects.filter(pregunta19__contains=obj[0],encuesta__year=year).count()
+					conteo = filtro.filter(actitud__pregunta19__contains=obj[0]).count()
 					pregunta19[obj[1]] = saca_porcentajes(conteo,encuestas,False)
 					
 				#tab practicas-------------------------------------------------------
 				#preg 23
 				pregunta23 = collections.OrderedDict()
 				for obj in CHOICE_23:
-					conteo = Practicas.objects.filter(pregunta23__contains=obj[0],encuesta__year=year).count()
+					conteo = filtro.filter(practicas__pregunta23__contains=obj[0]).count()
 					pregunta23[obj[1]] = saca_porcentajes(conteo,encuestas,False)
 				
 				#preg 24
 				pregunta24 = collections.OrderedDict()
 				for obj in CHOICE_24:
-					conteo = Practicas.objects.filter(pregunta24__contains=obj[0],encuesta__year=year).count()
+					conteo = filtro.filter(practicas__pregunta24__contains=obj[0]).count()
 					pregunta24[obj[1]] = saca_porcentajes(conteo,encuestas,False)
 
 				#tab persepcion-------------------------------------------------------
 				#preg 30
 				pregunta30 = collections.OrderedDict()
 				for obj in CHOICE_30:
-					conteo = Percepcion.objects.filter(pregunta30__contains=obj[0],encuesta__year=year).count()
+					conteo = filtro.filter(percepcion__pregunta30__contains=obj[0]).count()
 					pregunta30[obj[1]] = saca_porcentajes(conteo,encuestas,False)
 
 				#preg 31
 				pregunta31 = collections.OrderedDict()
 				for obj in CHOICE_31:
-					conteo = Percepcion.objects.filter(pregunta31__contains=obj[0],encuesta__year=year).count()
+					conteo = filtro.filter(percepcion__pregunta31__contains=obj[0]).count()
 					pregunta31[obj[1]] = saca_porcentajes(conteo,encuestas,False)
 
 				#tab estado actual-------------------------------------------------------
 				#preg 36
 				pregunta36 = collections.OrderedDict()
 				for obj in CHOICE_36:
-					conteo = EstadoActual.objects.filter(pregunta36__contains=obj[0],encuesta__year=year).count()
+					conteo = filtro.filter(estadoactual__pregunta36__contains=obj[0]).count()
 					pregunta36[obj[1]] = saca_porcentajes(conteo,encuestas,False)
 
 				#preg 37
 				pregunta37 = collections.OrderedDict()
 				for obj in CHOICE_37:
-					conteo = EstadoActual.objects.filter(pregunta37__contains=obj[0],encuesta__year=year).count()
+					conteo = filtro.filter(estadoactual__pregunta37__contains=obj[0]).count()
 					pregunta37[obj[1]] = saca_porcentajes(conteo,encuestas,False)
 					
 				#fin salidas-------------------------------------------------------
@@ -243,14 +260,55 @@ def index(request,template="violencia_juvenil/index.html"):
 	return render(request, template, locals())
 
 def saca_porcentajes(dato, total, formato=True):
-    if dato != None:
-        try:
-            porcentaje = (dato/float(total)) * 100 if total != None or total != 0 else 0
-        except:
-            return 0
-        if formato:
-            return porcentaje
-        else:
-            return '%.2f' % porcentaje
-    else:
-        return 0
+	if dato != None:
+		try:
+			porcentaje = (dato/float(total)) * 100 if total != None or total != 0 else 0
+		except:
+			return 0
+		if formato:
+			return porcentaje
+		else:
+			return '%.2f' % porcentaje
+	else:
+		return 0
+
+#ajax filtros
+def get_munis(request):
+    '''Metodo para obtener los municipios via Ajax segun los departamentos selectos'''
+    ids = request.GET.get('ids', '')
+    dicc = {}
+    resultado = []
+    if ids:
+        lista = ids.split(',')
+        for id in lista:
+            try:
+                departamento = Departamento.objects.get(pk=id)
+                municipios = Municipio.objects.filter(departamento__id=departamento.pk).order_by('nombre')
+                lista1 = []
+                for municipio in municipios:
+                    muni = {}
+                    muni['id'] = municipio.pk
+                    muni['nombre'] = municipio.nombre
+                    lista1.append(muni)
+                    dicc[departamento.nombre] = lista1
+            except:
+                pass
+
+    #filtrar segun la organizacion seleccionada
+    org_ids = request.GET.get('org_ids', '')
+    if org_ids:
+        lista = org_ids.split(',')
+        municipios = [encuesta.municipio for encuesta in Encuesta.objects.filter(organizacion__id__in=lista)]
+        #crear los keys en el dicc para evitar KeyError
+        for municipio in municipios:
+            dicc[municipio.departamento.nombre] = []
+
+        #agrupar municipios por departamento padre
+        for municipio in municipios:
+            muni = {'id': municipio.id, 'nombre': municipio.nombre}
+            if not muni in dicc[municipio.departamento.nombre]:
+                dicc[municipio.departamento.nombre].append(muni)
+
+    resultado.append(dicc)
+
+    return HttpResponse(simplejson.dumps(resultado), content_type='application/json')
