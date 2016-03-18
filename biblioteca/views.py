4c05414a3ebe64cda70c4a1ff5e083e91aa57eb5
shiminasai <pablo.ismael90@gmail.com>
2016-03-18 16:52:32 -0600

    diseno de biblioteca cambia ahora

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request,template='biblioteca/index.html',slug=None):
+	return render(request, template, locals())
+	
+def bdetalle(request,template='biblioteca/detalle.html',slug=None):
+	return render(request, template, locals())
