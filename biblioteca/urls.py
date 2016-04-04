d439288be314fdec9829167e668f8a1849cd606d
Carlos Rocha <crocha09.09@gmail.com>
2016-04-04 15:03:34 -0600

    agregando busqueda por tema

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('biblioteca.views',
 	url(r'^$', 'index', name='index-biblioteca'),
 	url(r'^detalle/(?P<slug>[-\w]+)/$', 'detalle_guia', name='detalle-guia'),
 	url(r'^busqueda/$', 'buscar_guia', name='buscar-guia'),
+	url(r'^tema/(?P<id>[0-9]+)/$', 'buscar_tema', name='buscar-tema'),
 
 )
